ec604c04dae619b92ba4e5d9b59c666c30161418
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-10-04 12:25:17 +0530

    Added ingredients in recipe

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,7 +10,7 @@ export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
--- src/app/recipes/services/recipe.service.ts
+++ src/app/recipes/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import {Recipe} from '../recipe.model'
 
 @Injectable({
@@ -7,8 +8,16 @@ import {Recipe} from '../recipe.model'
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe("Recipe 1", "Lovely food", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
-    , new Recipe("Recipe 2", "Another lovely food", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg")
+    new Recipe("Recipe 1", "Lovely food", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
+    [
+      new Ingredient("Chicken",2),
+      new Ingredient("lettuce",3)
+    ])
+    , new Recipe("Recipe 2", "Another lovely food", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg",
+    [
+      new Ingredient("Chicken",4),
+      new Ingredient("French Fries",10)
+    ])
   ];
    
   recipeSelected = new EventEmitter<Recipe>();
