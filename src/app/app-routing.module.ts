8bc0495ba969616782428063b3e2c812d5e50ed5
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-10-04 12:49:37 +0530

    Added router for component  rendering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeService } from './recipes/services/recipe.service';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/recipes", pathMatch:"full" },
+  { path: "recipes", component: RecipesComponent },
+  { path: "shopping-list", component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
