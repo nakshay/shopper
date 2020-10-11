5ba2ff127e9588744e78590db700e50a368d79f7
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-10-11 12:27:10 +0530

    Added authentication form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -17,7 +18,8 @@ const routes: Routes = [
     {path:":id",component: RecipeDetailComponent,resolve:[RecipeResolverService]},
     {path: ":id/edit", component : RecipeEditComponent,resolve:[RecipeResolverService]},
   ]},
-  { path: "shopping-list", component: ShoppingListComponent }
+  { path: "shopping-list", component: ShoppingListComponent },
+  {path:"auth",component:AuthComponent}
 ];
 
 @NgModule({
