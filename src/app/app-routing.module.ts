7b365af5f0f447736defcf3cb8b059b6e51e3b5d
Akshay Naik <inbox.akshaynaik@gmail.com>
2020-10-13 11:53:33 +0530

    created separate routes for recipe module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
 import { AuthComponent } from './auth/auth/auth.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeResolverService } from './recipes/services/recipe-resolver.service';
-import { RecipeService } from './recipes/services/recipe.service';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
 const routes: Routes = [
-  { path: "", redirectTo: "/recipes", pathMatch:"full" },
-  { path: "recipes", component: RecipesComponent,
-  canActivate: [AuthGuard],
-  children :[
-    { path:"" ,component: RecipeStartComponent },
-    {path: "new", component: RecipeEditComponent},
-    {path:":id",component: RecipeDetailComponent,resolve:[RecipeResolverService]},
-    {path: ":id/edit", component : RecipeEditComponent,resolve:[RecipeResolverService]},
-  ]},
   { path: "shopping-list", component: ShoppingListComponent },
   {path:"auth",component:AuthComponent}
 ];
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes.component';
+import { RecipeRoutingModule } from "./recipe-routing.module";
 
 @NgModule({
     declarations: [
@@ -18,16 +19,12 @@ import { RecipesComponent } from './recipes.component';
         RecipeStartComponent,
         RecipeEditComponent
     ],
-    imports:[RouterModule,CommonModule,ReactiveFormsModule],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
+    imports:[
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule,
+        RecipeRoutingModule
     ]
-
 })
 export class RecipesModule {
 
